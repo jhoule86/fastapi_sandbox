b022b4d1955a36948cdcd5da617194a4a513aa4d
Jeff Houle <jhoule86@gmail.com>
2025-09-15 19:08:41 -0400

    silence wget when downloading karate

--- get_karate.sh
+++ get_karate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget -O karate.jar https://github.com/karatelabs/karate/releases/download/v1.5.1/karate-1.5.1.jar
+wget -q -O karate.jar https://github.com/karatelabs/karate/releases/download/v1.5.1/karate-1.5.1.jar
