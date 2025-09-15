c8712ac9410337c2ee79f7357a0206849b2b811f
Jeff Houle <jhoule86@gmail.com>
2025-09-15 18:46:48 -0400

    fail step when karate tests fail

--- run_karate_tests.sh
+++ run_karate_tests.sh
@@ -8,7 +8,19 @@ fi
 # run the app server in the background
 sh run.sh &
 
-java -cp  karate.jar com.intuit.karate.Main test/karate/sample.feature
+if [ -f karate_results ]; then
+    rm karate_results
+fi
+
+java -cp  karate.jar com.intuit.karate.Main test/karate/ > karate_results
+cat ./karate_results
+
+echo Karate tests status: 
+if grep -q "failed: 0" ./karate_results; then
+    echo Passed
+else
+    echo Failed
+fi
 
 # kill everything including the app server
 trap "trap - TERM && kill -- -$$" INT TERM EXIT
