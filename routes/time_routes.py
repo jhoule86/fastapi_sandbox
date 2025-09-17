56c2b13719dd83d526acc1d93b61badaba9b5834
Jeff Houle <jhoule86@gmail.com>
2025-09-17 19:56:50 -0400

    document the hanging routes

--- routes/time_routes.py
+++ routes/time_routes.py
@@ -8,10 +8,20 @@ router = APIRouter(prefix='/time')
 
 @router.api_route("/hang/{sec}")
 async def hang(sec: int):
+    """
+    Wait for the prescribed amount of seconds,
+    then return a basic response
+    """
+    
     sleep(sec)
     return read_root()
 
 @router.api_route("/redirecthang/{sec}")
 async def redirecthang(sec: int):
+    """
+    Wait for the prescribed amount of seconds,
+    then redirect to an internal endpoint that returns a basic response
+    """
+    
     sleep(sec)
     return RedirectResponse("/json/sample")
